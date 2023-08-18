795f32dab8f890196e664d5aa44b3a9410e56f8c
Henning Berge <henning.ber@gmail.com>
2023-08-19 01:34:03 +0200

    add logo and links

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './fonts/LifeCraft_Font.ttf';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
