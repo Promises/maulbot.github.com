621c30ab803852800e0d2bd6da681a672967d192
Henning Berge <henning.ber@gmail.com>
2023-08-19 02:04:17 +0200

    add logo and links

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import styles from './App.module.scss';
+import DiscordButton from "./discord/discordButton";
 
 function App() {
     return (
-        <div className="App">
+        <div className={styles.app}>
             <header className="App-header">
                 <h1 className={styles.logo}><span>WarCraft</span> <span>Maul</span></h1>
             </header>
-            <main>
-                <div>Download latest version:
-                    <a href={'/Warcraft_Maul_Reimagined_v4.4.3.w3x'}>Download Now!</a></div>
+            <main className={styles.main}>
+                <div>
+                    <a className={'button green'} href={'/Warcraft_Maul_Reimagined_v4.4.3.w3x'}>Download lastest version v4.4.3</a></div>
+                <div className={styles.discord}><a href={'https://discord.gg/4Ng3VgE'}><DiscordButton/></a></div>
 
-                <div>Join the community on <a href={'https://discord.gg/4Ng3VgE'}>Discord</a></div>
-                <div>Contribute changes to the project on <a href={'https://github.com/Promises/Warcraft-Maul-Buildtools'}>Github</a></div>
 
             </main>
+            <div className={styles.github}><a href={'https://github.com/Promises/Warcraft-Maul-Buildtools'}><img
+                src={'/github-mark-white.svg'} alt={'Contribute changes to the project on Github'}/></a></div>
         </div>
     );
 }
